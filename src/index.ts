27dadb7be9abc29379849c38082910827c74131a
Arnau Espin <arnauespin@gmail.com>
2022-08-17 12:02:24 +0200

    feat: remove lib folder

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-export * from "./lib";
-
+export * from './update'
+export * from './upload'
+export * from "./mint"
+export * from "./enums"
+export * from "./interfaces"
+export * from "./types"
+export * from "./constants"
--- src/lib/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './update'
-export * from './upload'
-export * from "./mint"
-export * from "./enums"
-export * from "./interfaces"
-export * from "./types"
-export * from "./constants"
